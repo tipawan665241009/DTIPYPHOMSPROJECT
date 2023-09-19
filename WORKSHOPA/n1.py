7fdd76fd4ba5c47582825509283ef9f3cb2f21dc
tipawan665241009 <142291445+tipawan665241009@users.noreply.github.com>
2023-09-19 11:27:07 +0700

    Add files via upload

---
+++ WORKSHOPA/n1.py
@@ -0,0 +1,16 @@
+ print('Hello,\
+World')
+PRINT('Hi,Worid')
+PRINT('Hey, World.')
+print('hue, wotld')
+print("Hum, word")
+print("123456")
+print('''
+      DTI
+      ^0^
+      SAU
+      ''')
+print("""
+      ID : 6652410009
+      NAME : TIPAWAN
+   """)
---
+++ WORKSHOPA/test02.py
@@ -0,0 +1,12 @@
+print('Welcom to "SAU"',end='')
+print("Welcom to 'DTI'",end="")
+print("---------------")
+print('Welcome to \SAU\'')
+print("Welcom to\"DTI\"")
+print("_ _ _ _ _ _ _ _ _")
+print(4546)
+print(4547.0545)
+print(True)
+print(False)
+print(15 * 10 + 2 )# expression นิจพน์
+print(True or False and False)
